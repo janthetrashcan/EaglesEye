d43a08e1ff2521630c7b8cc3400619c08031b172
JR-Bam <110076336+JR-Bam@users.noreply.github.com>
2025-04-30 22:48:50 +0800

    Added SavedItem Database Model

---
+++ app/src/main/java/com/humanerrors/eagleseye/models/SavedItemModel.kt
@@ -0,0 +1,19 @@
+package com.humanerrors.eagleseye.models
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class SavedItemModel(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val title: String,
+
+    @ColumnInfo(name = "place_building")
+    val placeBuilding: String,
+    @ColumnInfo(name = "place_room")
+    val placeRoom: String?,
+    @ColumnInfo(name = "time_stamp")
+    val timeStamp: String // could be a better data type if we wanna sort/filter by time
+)
